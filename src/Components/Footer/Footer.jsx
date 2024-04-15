bbd61102ecdd754c11dc90373d86b7077caa1f74
Idba1 <moniraislamself@gmail.com>
2024-04-15 21:54:52 +0600

    update footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
                 <div className="container flex flex-col justify-between py-10 mx-auto space-y-8 lg:flex-row lg:space-y-0">
                     <div className="lg:w-1/3">
                         <a rel="noopener noreferrer" href="#" className="flex justify-center space-x-3 lg:justify-start">
-                            <div className="flex items-center justify-center w-12 h-12 rounded-full dark:bg-violet-600">
+                            <div className="flex items-center justify-center w-32 h-20 ">
                                 <img src="../../../public/logo.png" alt="" />
                             </div>
                             <span className="self-center text-2xl font-semibold text-[#0E46A3]">Brightest Luxury</span>
