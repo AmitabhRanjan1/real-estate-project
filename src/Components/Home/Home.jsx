1cf223d3f540e306db989cc2c4525e76520abf7a
Idba1 <moniraislamself@gmail.com>
2024-04-15 21:09:32 +0600

    complete hero section

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -6,48 +6,7 @@ const Home = () => {
     return (
         <>
             <div className="">
-                <Slider>St. Thomas Parish
-Saint Kitts and Nevis
-$4,950,000
-SEE DETAILS 
-Pause Video
-Affiliate Homepage listing image
-St. Jean
-St. Barthelemy
-$50,000,000
-SEE DETAILS 
-Pause Video
-Affiliate Homepage listing image
-Piedmont, CA
-United States
-$14,500,000
-SEE DETAILS 
-Pause Video
-Affiliate Homepage listing image
-San Francisco, CA
-United States
-$7,995,000
-SEE DETAILS 
-Pause Video
-Affiliate Homepage listing image
-St. Thomas Parish
-Saint Kitts and Nevis
-$4,950,000
-SEE DETAILS 
-Pause Video
-Affiliate Homepage listing image
-St. Jean
-St. Barthelemy
-$50,000,000
-SEE DETAILS 
-Pause Video
-Affiliate Homepage listing image
-Piedmont, CA
-United States
-$14,500,000
-SEE DETAILS 
-Pause Video
-</Slider>
+                <Slider></Slider>
             </div>
             <CardEstate></CardEstate>
         </>
--- src/Components/Home/Slider.jsx
+++ src/Components/Home/Slider.jsx
@@ -11,39 +11,39 @@ import 'swiper/css/scrollbar';
 import image from '../../../public/Slides.json'
 
 const Slider = () => {
-    [
-        {
-          "id": 1,
-          "img": "https://example.com/image1.jpg"
-        },
-        {
-          "id": 2,
-          "img": "https://example.com/image2.jpg"
-        },
-        {
-          "id": 3,
-          "img": "https://example.com/image3.jpg"
-        }
-      ]
+
     return (
         <div>
-             <Swiper
-      // install Swiper modules
-      modules={[Navigation, Pagination, Scrollbar, A11y]}
-      spaceBetween={50}
-      slidesPerView={1}
-      navigation
-      pagination={{ clickable: true }}
-      scrollbar={{ draggable: true }}
-      onSwiper={(swiper) => console.log(swiper)}
-      onSlideChange={() => console.log('slide change')}
-    >
-        {
-            image.map((img)=>(
-                <SwiperSlide key={img.id}><img className='w-screen h-96' src={img.img} alt="" />jjj</SwiperSlide>
-            ))
-        }
-    </Swiper>
+            <Swiper
+                // install Swiper modules
+                modules={[Navigation, Pagination, Scrollbar, A11y]}
+                spaceBetween={50}
+                slidesPerView={1}
+                navigation
+                pagination={{ clickable: true }}
+                scrollbar={{ draggable: true }}
+                onSwiper={(swiper) => console.log(swiper)}
+                onSlideChange={() => console.log('slide change')}
+            >
+                
+                {
+
+                    image.map((img) => (
+                        
+                        <SwiperSlide key={img.id}>
+                            <div className="relative">
+                            <img className='w-screen h-96' src={img.img} alt="" />
+                            <div className="absolute top-14 left-5 lg:top-20  lg:left-60 lg:p-4 text-sky-950 p-6 ">
+                                <h2 className="text-3xl text-sky-950 font-bold">{img.title}</h2>
+                                <p className="text-base  font-bold mt-2">{img.description}</p>
+                                <button className='bg-[#E1F7F5] text-sky-950 btn mt-4 font-bold text-base'>View</button>
+                            </div>
+                        </div>
+                            
+                            </SwiperSlide>
+                    ))
+                }
+            </Swiper>
         </div>
     );
 };
