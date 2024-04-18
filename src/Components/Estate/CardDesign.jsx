278a8d37c27b72fe0d8e12f976780230497ff20a
Idba1 <moniraislamself@gmail.com>
2024-04-18 19:53:36 +0600

    add aos animation

--- src/Components/Estate/CardDesign.jsx
+++ src/Components/Estate/CardDesign.jsx
@@ -1,5 +1,10 @@
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
+// ..
+AOS.init();
+
 const CardDesign = ({ luxury }) => {
     const {  estate_title, description, price,
         image_url} = luxury;
@@ -8,8 +13,8 @@ const CardDesign = ({ luxury }) => {
         <div>
         <Link to={`/buy/details/property`}>
             <div className=" bg-base-100 shadow-lg">
-                <figure><img className='w-full rounded-lg h-64' src={image_url} alt="Movie" /></figure>
-                <div className="card-body">
+                <figure><img  data-aos="zoom-in" data-aos-duration="2000" className='w-full rounded-lg h-64' src={image_url} alt="Movie" /></figure>
+                <div data-aos="zoom-in-right" data-aos-duration="1000" className="card-body">
                     <h2 className="card-title text-[#1E0342]">{estate_title}</h2>
                     <p>{description}</p>
                     <p>Price: {price}</p>
